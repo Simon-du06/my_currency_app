3213dac0cde3b6f739451184e2a5cecb3960b86b
Simon Puccio <simon.puccio@epitech.eu>
2025-01-11 22:30:54 +0100

    [+] You can now set arrival currency but UI is not looking pretty

--- components/AmountInput.tsx
+++ components/AmountInput.tsx
@@ -1,23 +1,27 @@
 import React, {useEffect, useState} from 'react';
-import { Text, TextInput, View, Button, Image } from "react-native";
-import { DollarConvertion } from '../utils/currency';
-import { styles } from '../styles/styles';
-import { get_exchange } from '../utils/api'
+import { Text, TextInput, View, Image } from "react-native";
+import { SelectList } from 'react-native-dropdown-select-list'
 
 import images from "@/constants/icons";
+import { currencyData } from '@/constants/currencies';
+import { get_exchange } from '@/utils/api'
+import { DollarConvertion } from '@/utils/currency';
 
 const AmountInput = () => {
-  const [amount, setText] = useState('');
+  const [amount, setAmount] = useState('');
   const [convertedAmount, setConvertedAmount] = useState(0.0);
   const [rate, setRate] = useState('');
+  const [selected, setSelected] = useState("USD");
 
   useEffect(() => {
     get_exchange(setRate);
   }, []);
 
-  const handleAmountChange = (value: string) => {
+  const handleAmountChange = (value: string, currency: string) => {
+    setAmount(value);
+    setSelected(currency)
     const obj = JSON.parse(rate);
-    setConvertedAmount(parseFloat(DollarConvertion(value, obj.rates.USD)));
+    setConvertedAmount(parseFloat(DollarConvertion(value, obj.rates[currency])));
   };
 
   return (
@@ -27,11 +31,11 @@ const AmountInput = () => {
         <TextInput
           className='flex-1 font-lato-bold text-4xl text-left'
           placeholder="Enter amount in €"
-          onChangeText={handleAmountChange}
+          onChangeText={(value) => handleAmountChange(value, selected)}
           defaultValue={amount}
           keyboardType="numeric" />
         <Image source={images.euro} className='w-10 h-10' />
-        <Text className='font-lato-bold text-xl ml-2'>EUR</Text>
+        {/* <Text className='font-lato-bold text-xl ml-2'>EUR</Text> */}
       </View>
     </View>
     <View className="flex flex-col mt-2 justify-center rounded-3xl w-96 border-2 border-ternary px-6 py-2">
@@ -41,7 +45,12 @@ const AmountInput = () => {
           {isNaN(convertedAmount) ? 0 : convertedAmount}$
         </Text>
       <Image source={images.usa} className='w-10 h-10' />
-      <Text className='font-lato-bold text-xl ml-2'>USD</Text>
+      {/* <Text className='font-lato-bold text-xl ml-2'>USD</Text> */}
+      <SelectList
+          setSelected={(currency : string) => handleAmountChange(amount, currency)}
+          data={currencyData}
+          save="value"
+        />
       </View>
     </View></>
   );
---
+++ constants/currencies.ts
@@ -0,0 +1,10 @@
+export const currencyData = [
+    { key: '1', value: 'USD' },
+    { key: '2', value: 'EUR' },
+    { key: '3', value: 'GBP' },
+    { key: '4', value: 'JPY' },
+    { key: '5', value: 'AUD' },
+    { key: '6', value: 'CAD' },
+    { key: '7', value: 'CHF' },
+    { key: '8', value: 'CNH'}
+  ];
