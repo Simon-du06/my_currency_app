d6833606a365cdd4143034e27d144f2091ccd1bd
Simon Puccio <simon.puccio@epitech.eu>
2025-01-11 13:11:28 +0100

    [+] Add TypeScript declarations for image file imports and update icon imports

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,4 +1,23 @@
-import change from "@/assets/images/change.png";
+import switchicon from "@/assets/images/change.png";
 import bell from "@/assets/images/bell.png";
 import graph from "@/assets/images/graph.png";
 import profile from "@/assets/images/profile.png";
+import rate_up from "@/assets/images/rate_up.png";
+import rate_down from "@/assets/images/down.png";
+import rate_down2 from "@/assets/images/down2.png";
+import closedArrow from "@/assets/images/Closed.png";
+import closedupArrow from "@/assets/images/Closed_up.png";
+import openArrow from "@/assets/images/Open.png";
+
+export default {
+    switchicon,
+    bell,
+    graph,
+    profile,
+    rate_up,
+    rate_down,
+    rate_down2,
+    closedArrow,
+    closedupArrow,
+    openArrow,
+};
